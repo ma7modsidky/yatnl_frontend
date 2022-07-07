cd81f8b005ceca9fcc90abd509cb9f05d5066fc0
Mahmoud <ma7mod_a@live.com>
2022-07-07 23:17:10 +0200

    axios baseurl

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = "http://yatnl.pythonanywhere.com/api/";
+const baseURL = "https://yatnl.pythonanywhere.com/api/";
 // const baseURL = 'http://127.0.0.1:8000/api/'
 
 
